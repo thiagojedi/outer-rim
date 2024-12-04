b87cde0fe85a2bd6b0d05bb51f0c47ab564f6163
Thiago Cerqueira <thiago.cerqueira@motorolasolutions.com>
2024-12-04 16:48:36 -0300

    Creating new user

--- src/islands/UserForm.tsx
+++ src/islands/UserForm.tsx
@@ -5,9 +5,9 @@ import { useSignal } from "@preact/signals";
 export const UserForm: FunctionComponent<{ method: "POST" | "PATCH" }> = (
   { method },
 ) => {
-  const handle = useSignal("username");
-  const name = useSignal("Display name");
-  const bio = useSignal("Profile bio");
+  const handle = useSignal("");
+  const name = useSignal("");
+  const bio = useSignal("");
 
   return (
     <form method={method} className="form">
@@ -19,7 +19,7 @@ export const UserForm: FunctionComponent<{ method: "POST" | "PATCH" }> = (
             className="input"
             name="handle"
             placeholder="username"
-            pattern="[a-z0-9_]"
+            pattern="[a-z0-9_]+"
             onChange={(e) => handle.value = e.currentTarget.value}
           />
           <span className="icon is-small is-left">
@@ -41,55 +41,55 @@ export const UserForm: FunctionComponent<{ method: "POST" | "PATCH" }> = (
         </div>
 
         <div className="column">
-          <div className="field is-grouped">
-            <div className="file is-boxed">
-              <label className="file-label">
-                <input className="file-input" type="file" name="header" />
-                <span className="file-cta">
-                  <span className="file-icon">
-                    <i className="fas fa-upload"></i>
-                  </span>
-                  <span className="file-label">Header</span>
-                </span>
-              </label>
-            </div>
+          {/*<div className="field is-grouped">*/}
+          {/*  <div className="file is-boxed">*/}
+          {/*    <label className="file-label">*/}
+          {/*      <input className="file-input" type="file" name="header" />*/}
+          {/*      <span className="file-cta">*/}
+          {/*        <span className="file-icon">*/}
+          {/*          <i className="fas fa-upload"></i>*/}
+          {/*        </span>*/}
+          {/*        <span className="file-label">Header</span>*/}
+          {/*      </span>*/}
+          {/*    </label>*/}
+          {/*  </div>*/}
 
-            <div className="control is-expanded">
-              <div className="control">
-                <textarea
-                  className="input"
-                  name="headerDescription"
-                  placeholder="Header description (optional)"
-                />
-              </div>
-              <p className="help">600x200 images are best</p>
-            </div>
-          </div>
+          {/*  <div className="control is-expanded">*/}
+          {/*    <div className="control">*/}
+          {/*      <textarea*/}
+          {/*        className="input"*/}
+          {/*        name="headerDescription"*/}
+          {/*        placeholder="Header description (optional)"*/}
+          {/*      />*/}
+          {/*    </div>*/}
+          {/*    <p className="help">600x200 images are best</p>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
 
-          <div className="field is-grouped">
-            <div className="file is-boxed">
-              <label className="file-label">
-                <input className="file-input" type="file" name="avatar" />
-                <span className="file-cta">
-                  <span className="file-icon">
-                    <i className="fas fa-upload"></i>
-                  </span>
-                  <span className="file-label">Avatar</span>
-                </span>
-              </label>
-            </div>
+          {/*<div className="field is-grouped">*/}
+          {/*  <div className="file is-boxed">*/}
+          {/*    <label className="file-label">*/}
+          {/*      <input className="file-input" type="file" name="avatar" />*/}
+          {/*      <span className="file-cta">*/}
+          {/*        <span className="file-icon">*/}
+          {/*          <i className="fas fa-upload"></i>*/}
+          {/*        </span>*/}
+          {/*        <span className="file-label">Avatar</span>*/}
+          {/*      </span>*/}
+          {/*    </label>*/}
+          {/*  </div>*/}
 
-            <div className="control is-expanded">
-              <div className="control">
-                <textarea
-                  className="input"
-                  name="avatarDescription"
-                  placeholder="Avatar description (optional)"
-                />
-              </div>
-              <p className="help">1:1 images are best</p>
-            </div>
-          </div>
+          {/*  <div className="control is-expanded">*/}
+          {/*    <div className="control">*/}
+          {/*      <textarea*/}
+          {/*        className="input"*/}
+          {/*        name="avatarDescription"*/}
+          {/*        placeholder="Avatar description (optional)"*/}
+          {/*      />*/}
+          {/*    </div>*/}
+          {/*    <p className="help">1:1 images are best</p>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
 
           <div className="field">
             <div className="control">
@@ -105,24 +105,24 @@ export const UserForm: FunctionComponent<{ method: "POST" | "PATCH" }> = (
             </div>
           </div>
 
-          <div className="field">
-            <div className="control">
-              <label htmlFor="Bio" className="label">Bio</label>
-              <div className="control">
-                <textarea
-                  name="bio"
-                  className="input"
-                  onChange={(e) => bio.value = e.currentTarget.value}
-                />
-              </div>
-            </div>
-          </div>
+          {/*<div className="field">*/}
+          {/*  <div className="control">*/}
+          {/*    <label htmlFor="bio" className="label">Bio</label>*/}
+          {/*    <div className="control">*/}
+          {/*      <textarea*/}
+          {/*        name="bio"*/}
+          {/*        className="input"*/}
+          {/*        onChange={(e) => bio.value = e.currentTarget.value}*/}
+          {/*      />*/}
+          {/*    </div>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
 
-          <div className="checkboxes">
-            <label className="checkbox">
-              <input name="bot" type="checkbox" /> Is Bot?
-            </label>
-          </div>
+          {/*<div className="checkboxes">*/}
+          {/*  <label className="checkbox">*/}
+          {/*    <input name="bot" type="checkbox" /> Is Bot?*/}
+          {/*  </label>*/}
+          {/*</div>*/}
 
           <br />
 
--- src/routes/(private)/settings/new-user.tsx
+++ src/routes/(private)/settings/new-user.tsx
@@ -1,16 +1,34 @@
 import { define } from "../../../utils.ts";
 import { UserForm } from "../../../islands/UserForm.tsx";
 import { page } from "fresh";
+import { createActor } from "../../../federation/repositories/actor.ts";
+import federation from "../../../federation/mod.ts";
 
 export const handler = define.handlers({
-  POST: async (ctx) => {
-    const formData = await ctx.req.formData();
+  POST: async ({ redirect, req, url }) => {
+    const formData = await req.formData();
 
-    const fromEntries = Object.fromEntries(formData.entries());
+    const context = federation.createContext(req, undefined);
 
-    console.log(fromEntries);
+    const username = formData.get("handle") as string | null;
 
-    return page();
+    if (!username) {
+      return page({ error: "handle" });
+    }
+
+    const name = formData.get("name") as string | null;
+
+    await createActor({
+      userId: 1,
+      uri: context.getActorUri(username).href,
+      name: name ?? username,
+      handle: `@${username}@${url.host}`,
+      inboxUrl: context.getInboxUri(username),
+      sharedInboxUrl: context.getInboxUri(),
+      url: context.getActorUri(username),
+    });
+
+    return redirect("/settings");
   },
 });
 
